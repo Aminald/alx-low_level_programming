6598ed92550d7630f6c7b8a281a392247f7afeb2
Aminald <laghidia@gmail.com>
2023-04-30 12:32:07 -0700

    task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * main - tests whether the function positive_or_negative() correctly
  *        identifies if a number is positive, negative, or zero
  *
  * Return: Always 0
  */
+
 int main(void)
 {
 int i;
