341721a74534ed084c53504b2be2909c3a8d14a9
Aminald <laghidia@gmail.com>
2023-04-30 15:52:32 -0700

    task3

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,50 +1,30 @@
 #include "main.h"
 /**
-* convert_day - converts day of month to day of year, without accounting
-* for leap year
-* @month: month in number format
-* @day: day of month
-* Return: day of year
-*/
+ * convert_day - converts day of month to day of year, without accounting
+ * for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
 int convert_day(int month, int day)
 {
-switch (month)
+int i, days;
+days = 0;
+for (i = 1; i < month; i++)
 {
-case 2:
-day = 31 + day;
-break;
-case 3:
-day = 59 + day;
-break;
-case 4:
-day = 90 + day;
-break;
-case 5:
-day = 120 + day;
-break;
-case 6:
-day = 151 + day;
-break;
-case 7:
-day = 181 + day;
-break;
-case 8:
-day = 212 + day;
-break;
-case 9:
-day = 243 + day;
-break;
-case 10:
-day = 273 + day;
-break;
-case 11:
-day = 304 + day;
-break;
-case 12:
-day = 334 + day;
-break;
-default:
-break;
+if (i == 2)
+{
+days += 28;
 }
-return (day);
+else if (i == 4 || i == 6 || i == 9 || i == 11)
+{
+days += 30;
+}
+else
+{
+days += 31;
+}
+}
+days += day;
+return (days);
 }
