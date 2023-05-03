3ff2e5649fc84423a3fec7034881937126516043
Aminald <laghidia@gmail.com>
2023-05-02 20:38:15 -0700

    tqsk11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,37 +1,38 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 #include <time.h>
-#include "main.h"
+#include <unistd.h>
+int _putchar(char c);
+
 /**
- * generate_password - Generates a random password
+ * main - generates a random valid password for the program 101-crackme
  *
- * Return: A pointer to the generated password string
- */
-char *generate_password(void)
-{
-static char password[7];
-int i;
-srand(time(NULL));
-for (i = 0; i < 6; i++)
-{
-password[i] = rand() % 94 + 33;
-}
-password[6] = '\0';
-return password;
-}
-/**
- * main - Generates and tests random passwords for 101-crackme
- *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-char *password;
-char command[50];
-do {
-password = generate_password();
-sprintf(command, "./101-crackme \"%s\"", password);
-} while (system(command) != 0);
-printf("Valid password found: %s\n", password);
-return 0;
+    int password[4];
+    int i, sum;
+    srand(time(NULL));
+
+    do {
+        sum = 0;
+        for (i = 0; i < 4; i++) {
+            password[i] = rand() % 256;
+            sum += password[i];
+        }
+    } while (sum != 0x10);
+
+    for (i = 0; i < 4; i++) {
+        _putchar(password[i]);
+    }
+    _putchar('\n');
+
+    return (0);
 }
+
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+
