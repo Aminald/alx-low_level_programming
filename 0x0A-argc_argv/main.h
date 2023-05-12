f83b689196b17b29dc20fd47894e63fb960dbfc7
Aminald <laghidia@gmail.com>
2023-05-12 09:19:47 -0700

    task1

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -5,4 +5,5 @@ int _atoi(char *s);
 int main(int argc, char *argv[]);
 int main(int argc __attribute__((unused)), char *argv[]);
 int check_num(char *str);
-#endif
+int main(int argc, char *argv[]);
+#endif /* MAIN_H */
