5e0dbd9ccd65fbdaaaa44e606282c96d1288d041
Aminald <laghidia@gmail.com>
2023-05-22 15:48:51 -0700

    tasl2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include "2-file_name.h"
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the name of the file it was compiled from.
+ *              Uses the PRINT_FILE_NAME macro defined in "2-file_name.h".
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 PRINT_FILE_NAME;
