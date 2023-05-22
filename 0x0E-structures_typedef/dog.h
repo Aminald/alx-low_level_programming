8dd497b59b4ca4f1072f846eb833be71ade237ca
Aminald <laghidia@gmail.com>
2023-05-22 16:03:39 -0700

    task1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Structure representing a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+/* Function prototype */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
