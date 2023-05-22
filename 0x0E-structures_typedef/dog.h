ae9cd130623862640c2b61081c4cd1a055034435
Aminald <laghidia@gmail.com>
2023-05-22 16:48:46 -0700

    task3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,4 +22,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
  * If d is NULL, print nothing.
  */
 void print_dog(struct dog *d);
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 #endif /* DOG_H */
