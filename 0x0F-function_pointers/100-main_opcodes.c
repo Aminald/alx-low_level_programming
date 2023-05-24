8a1427f851eab06ad90ae22c625c851fb47b38f2
Aminald <laghidia@gmail.com>
2023-05-24 15:06:50 -0700

    task4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,32 +11,26 @@
  */
 int main(int argc, char **argv)
 {
-    int index = 0, num_bytes;
-
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
-
-    num_bytes = atoi(argv[1]);
-
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        return 2;
-    }
-
-    unsigned char *main_ptr = (unsigned char *)main;
-
-    while (index < num_bytes)
-    {
-        printf("%02hhx", main_ptr[index]);
-        if (num_bytes > index + 1)
-            printf(" ");
-        index++;
-    }
-
-    printf("\n");
-    return 0;
+int index = 0, num_bytes;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+num_bytes = atoi(argv[1]);
+if (num_bytes < 0)
+{
+printf("Error\n");
+return (2);
+}
+unsigned char *main_ptr = (unsigned char *)main;
+while (index < num_bytes)
+{
+printf("%02hhx", main_ptr[index]);
+if (num_bytes > index + 1)
+printf(" ");
+index++;
+}
+printf("\n");
+return (0);
 }
